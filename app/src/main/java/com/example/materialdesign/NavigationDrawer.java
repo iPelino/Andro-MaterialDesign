a2cedd45e3b0d7bb9bfcc4be8a3c4ee8479bb614
Pelin Mutanguha <pelmut2000@gmail.com>
2019-04-04 16:11:33 +0200

    added the toggle button for the navigation drawer

--- app/src/main/java/com/example/materialdesign/NavigationDrawer.java
+++ app/src/main/java/com/example/materialdesign/NavigationDrawer.java
@@ -1,5 +1,8 @@
 package com.example.materialdesign;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,6 +10,8 @@ import android.support.v7.widget.Toolbar;
 public class NavigationDrawer extends AppCompatActivity {
 
     Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,14 @@ public class NavigationDrawer extends AppCompatActivity {
 
         toolbar = findViewById(R.id.navigationToolbar);
         setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open_drawer,R.string.close_drawer);
+        drawerLayout.setDrawerListener(toggle);
+
+        toggle.syncState();
 
     }
 }
